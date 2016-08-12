b0a2982faf990bc804b59ead7d13bf92cdc714b0
Kimberly Tze <kimberly.tze@originate.com>
2016-08-12 10:42:40 -0700

    Restarting after game over works

--- game.py
+++ game.py
@@ -4,9 +4,6 @@ import time
 import sys
 
 class EventBasedAnimationClass(object):
-    def onMousePressed(self, event):
-        self.mouseText = "last mousePressed: " + str((event.x, event.y))
-
     def validKey(self, key):
         return (key in self.validSyms)
 
@@ -144,11 +141,14 @@ class EventBasedAnimationClass(object):
 
     def runCommand(self, command):
         self.keyText = ""
-        if self.macaron:
+        if self.lose:
+            if self.timerCounter >= self.lose and command == "yes":
+                self.reset()
+        elif self.macaron:
             if command == "continue":
                 self.macaron += 1
                 if self.macaron - 2 == len(self.macaronText):
-                    self.gameOver("Macaron")
+                    self.gameOver("macaron")
                 else:
                     self.response = self.macaronText[self.macaron - 2]
             else:
@@ -200,7 +200,6 @@ class EventBasedAnimationClass(object):
         else:
             self.response = "I don't know how to " + command + "."
 
-
         self.redrawAll()
 
     def onTimerFired(self):
@@ -209,7 +208,7 @@ class EventBasedAnimationClass(object):
         if self.ovenTimer:
             self.ovenTimer += 1
             if self.ovenTimer == 55:
-                self.gameOver("Fire")
+                self.gameOver("fire")
             elif self.ovenTimer == 20:
                 self.response = "The oven is ready to go."
                 self.preheated = True
@@ -221,6 +220,8 @@ class EventBasedAnimationClass(object):
             elif self.cookieTimer == 31:
                 self.canvas.data["oven"] = PhotoImage(file="oven_burnt.gif")
                 self.gameOver("burnt")
+        if self.lose and self.timerCounter == self.lose:
+            self.response = "Play again? Type 'yes' to restart."
         if self.minute == 59:
             self.hour += 1
             self.minute = 0
@@ -253,14 +254,13 @@ class EventBasedAnimationClass(object):
         self.canvas.create_image(500, 500, image=self.canvas.data["fire"])
         self.canvas.create_rectangle(0, 840, 999, 999, fill = "black")
 
-        entered_text = self.canvas.create_text(20,968,text=self.carat + self.keyText, fill="white", anchor=W, font=('Helvetica', 20))
+        entered_text = self.canvas.create_text(20,968,text="> " + self.keyText, fill="white", anchor=W, font=('Helvetica', 20))
         self.canvas.create_text(20,860,text=self.response, fill="white", anchor=NW, font=('Helvetica', 20))
 
         x0 = self.canvas.bbox(entered_text)[0]
         x1 = self.canvas.bbox(entered_text)[2]
 
         self.canvas.create_text(150,80,text=self.timerText)
-        self.canvas.create_text(150,40,text=self.mouseText)
 
         if self.timerCounter % 4 == 1 or self.timerCounter % 4 == 0:
             self.canvas.create_line(20+x1-x0, 955, 20+x1-x0, 980, fill="white")
@@ -294,7 +294,6 @@ class EventBasedAnimationClass(object):
         self.canvas.create_line(cx, cy, minuteX, minuteY, fill="black", width=1)
 
     def initAnimation(self):
-        self.mouseText = "No mousePresses yet"
         self.keyText = ""
         self.timerText = "No timerFired calls yet"
         self.timerCounter = 0
@@ -302,17 +301,12 @@ class EventBasedAnimationClass(object):
     def __init__(self, width=1000, height=1000):
         self.width = width
         self.height = height
-        self.win = False
-        self.timerCounter = 0
-        self.cookieTimer = 0
         self.root = Tk()
-        self.bowl = []
-        self.recipe = ["butter", "chocolate", "egg2", "egg3", "flour", "sugar"]
-        self.eggsLeft = 3
-        self.preheated = False
-        self.timerDelay = 250 # in milliseconds (set to None to turn off timer)
+        self.timerDelay = 250  # in milliseconds (set to None to turn off timer)
         self.canvas = Canvas(self.root, width=1000, height=1000)
-        self.mixed = False
+        self.recipe = ["butter", "chocolate", "egg2", "egg3", "flour", "sugar"]
+        self.validSyms = ["space"] + [chr(x) for x in range(48, 58)] + [chr(x) for x in range(97, 123)]
+
         self.canvas.data = {
             "oven": PhotoImage(file="base_oven.gif"),
             "fire": "",
@@ -335,43 +329,62 @@ class EventBasedAnimationClass(object):
         self.response = "Let's bake some cookies!"
         self.keyText = ""
         self.item = ""
+        self.cookieTimer = 0
+        self.ovenTimer = 0
+        self.timerCounter = 0
+        self.macaron = 0
         self.minute = 0
         self.hour = 12
-        self.carat = "> "
-        self.ovenTimer = 0
-        self.validSyms = ["space"] + [chr(x) for x in range(48, 58)] + [chr(x) for x in range(97, 123)]
+        self.lose = 0
+        self.bowl = []
+        self.eggsLeft = 3
         self.bookOpened = False
+        self.mixed = False
+        self.preheated = False
         self.scooped = False
-        self.macaron = 0
-        self.macaronText = [
-            "How to make macarons:",
-            "Preheat the oven to 300 degrees F using the convection setting.",
-            "Line 3 baking sheets with silicone mats.",
-            "Measure the confectioners' sugar and almond flour by spooning them into measuring cups and leveling with \n a knife.",
-            "Transfer to a bowl; whisk to combine to combine.",
-            "Sift the sugar-almond flour mixture, a little at a time, through a fine-mesh sieve into a large bowl, pressing \n with a rubber spatula to pass through as must as possible.",
-            "It will take a while, and up to 2 tablespoons of coarse almond flour may be left; just toss it.",
-            "Beat the egg whites, cream of tartar and salt with a mixer on medium speed until frothy.",
-            "Increase the speed to medium high; gradually add the superfine sugar and beat until stiff and shiny, about 5 \n more minutes.",
-            "Transfer the batter to a pastry bag fitted with a 1/4-inch round tip.",
-            "Holding the bag vertically and close to the baking sheet, pipe 1 1/4-inch circles (24 per sheet).",
-            "Firmly tap the backing sheets twice against the counter to release any air bubbles.",
-            "Let the cookies sit at room temperature until the tops are no longer sticky to the touch, 15 minutes to 1 hour, \n depending on the humidity.",
-            "Slip another baking sheet under the first batch (a double baking sheet protects the cookies from the heat).",
-            "Bake the first batch until the cookies are shiny and rise 1/8 inch to form a 'foot,' about 20 minutes.",
-            "Transfer to a rack to cool completely.",
-            "Peel the cookies off the mats and sandwich a thin layer of filling."
-        ]
+        self.win = False
 
-        self.mouseText = "hello"
         self.timerText = "no time"
 
         self.redrawAll()
 
-    def onMousePressedWrapper(self, event):
-        if (not self._isRunning): return
-        self.onMousePressed(event)
-        self.redrawAll()
+    def reset(self):
+        self.canvas.data = {
+            "oven": PhotoImage(file="base_oven.gif"),
+            "fire": "",
+            "clock": PhotoImage(file="clock.gif"),
+            "book": PhotoImage(file="book_counter.gif"),
+            "bowl": PhotoImage(file="bowl.gif"),
+            "butter": PhotoImage(file="butter.gif"),
+            "chocolate": PhotoImage(file="chocolate.gif"),
+            "counter": PhotoImage(file="counter.gif"),
+            "egg1": PhotoImage(file="egg1.gif"),
+            "egg2": PhotoImage(file="egg2.gif"),
+            "egg3": PhotoImage(file="egg3.gif"),
+            "flour": PhotoImage(file="flour.gif"),
+            "girl": PhotoImage(file="girl.gif"),
+            "milk": PhotoImage(file="milk.gif"),
+            "sheet": PhotoImage(file="sheet.gif"),
+            "spoon": PhotoImage(file="spoon.gif"),
+            "sugar": PhotoImage(file="sugar.gif")
+        }
+        self.response = "Let's bake some cookies!"
+        self.keyText = ""
+        self.item = ""
+        self.cookieTimer = 0
+        self.ovenTimer = 0
+        self.timerCounter = 0
+        self.macaron = 0
+        self.minute = 0
+        self.hour = 12
+        self.lose = 0
+        self.bowl = []
+        self.eggsLeft = 3
+        self.bookOpened = False
+        self.mixed = False
+        self.preheated = False
+        self.scooped = False
+        self.win = False
 
     def onKeyPressedWrapper(self, event):
         if (not self._isRunning): return
@@ -387,16 +400,24 @@ class EventBasedAnimationClass(object):
         self.canvas.after(self.timerDelay, self.onTimerFiredWrapper)
 
     def gameOver(self, string):
-        if string == "Fire":
+        self.lose = self.timerCounter + 25
+        if string == "fire":
             self.canvas.data["fire"] = PhotoImage(file="fire.gif")
             self.response = "You set the house on fire..."
-        elif string == "Macaron":
+        elif string == "macaron":
             self.response = "You spent all day reading the macaron recipe..."
         elif string == "time":
             self.response = "You wasted all your time... how are you so slow?"
-        self.redrawAll()
-        # time.sleep(10)
-        self.response = "Play again? Type 'yes' to restart."
+        elif string == "milk":
+            self.response = ""
+        elif string == "ingredients":
+            self.response = ""
+        elif string == "burnt":
+            self.response = ""
+        elif string == "egg":
+            self.response = ""
+        elif string == "bowl":
+            self.response = ""
 
         self.redrawAll()
 
@@ -420,10 +441,6 @@ class EventBasedAnimationClass(object):
         self.root.protocol("WM_DELETE_WINDOW", lambda: self.quit())
         self._isRunning = True
         self.runningInIDLE =  ("idlelib" in sys.modules)
-        # DK: You can use a local function with a closure
-        # to store the canvas binding, like this:
-        def f(event): self.onMousePressedWrapper(event)
-        self.root.bind("<Button-1>", f)
         # DK: Or you can just use an anonymous lamdba function, like this:
         self.root.bind("<Key>", lambda event: self.onKeyPressedWrapper(event))
         self.onTimerFiredWrapper()
